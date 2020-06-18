f8c70242fb021a37bfd9dae1b780d2dac2534c40
Jina Zhu <jinazhu87@gmail.com>
2020-06-18 09:24:19 -0700

    enable search

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const auth = "563492ad6f91700001000001eab75c050171469d93186c19e531ae08";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
 
-async function curatedPhotos() {
+//event listeners
+
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
+
+// async function curatedPhotos() {
+//   const dataFetch = await fetch(
+//     "https://api.pexels.com/v1/curated?per_page=15&page=1",
+//     {
+//       //what we want to do with the data
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         Authorization: auth,
+//       },
+//     }
+//   );
+//   const data = await dataFetch.json();
+//   data.photos.forEach((photo) => {
+//     const galleryImg = document.createElement("div");
+//     galleryImg.classList.add("gallery-img");
+//     galleryImg.innerHTML = `<img src=${photo.src.large}> </img
+//       <p>${photo.photographer}<p>
+//       `;
+//     gallery.appendChild(galleryImg);
+//   });
+// }
+
+async function searchPhotos(query) {
   const dataFetch = await fetch(
-    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
     {
-      //what we want to do with the data
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -17,7 +51,13 @@ async function curatedPhotos() {
     }
   );
   const data = await dataFetch.json();
-  console.log(data);
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}/><p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
 }
 
-curatedPhotos();
+searchPhotos();
+// curatedPhotos();
